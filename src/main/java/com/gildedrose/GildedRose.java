544b7bac6b0f51f3a3eb0e8944532023eafb3652
peter-verstraete <peter.verstraete@continuum.be>
2019-05-27 19:24:00 +0200

    Kata Gilded Rose story 2

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,57 +9,74 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!isOfAgedBrieCategory(item)
-                    && !isOfBackstagePassesCategory(item)) {
-                if (qualityIsNotNegative(item)) {
-                    if (!isOfSulfurasCategory(item)) {
-                        decreaseQualityByOne(item);
-                    }
-                }
+            if (isOfAgedBrieCategory(item)) {
+                handleAgedBrieItem(item);
+            } else if (isOfSulfurasCategory(item)) {
+                handleSulfurasItem(item);
+            } else if (isOfBackstagePassesCategory(item)) {
+                handleBackstagePassesItem(item);
             } else {
-                if (qualityIsNotMoreThen50(item)) {
-                    increaseQualityByOne(item);
-
-                    if (isOfBackstagePassesCategory(item)) {
-                        if (sellIn10DaysOrLess(item)) {
-                            if (qualityIsNotMoreThen50(item)) {
-                                increaseQualityByOne(item);
-                            }
-                        }
-
-                        if (sellIn5DaysOrLess(item)) {
-                            if (qualityIsNotMoreThen50(item)) {
-                                increaseQualityByOne(item);
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!isOfSulfurasCategory(item)) {
-                decreaseSellInByOne(item);
-            }
-
-            if (sellByDateHasPassed(item)) {
-                if (!isOfAgedBrieCategory(item)) {
-                    if (!isOfBackstagePassesCategory(item)) {
-                        if (qualityIsNotNegative(item)) {
-                            if (!isOfSulfurasCategory(item)) {
-                                decreaseQualityByOne(item);
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (qualityIsNotMoreThen50(item)) {
-                        increaseQualityByOne(item);
-                    }
-                }
+                handleDefaultItem(item);
             }
         }
     }
 
+    // Handling commands
+    private void handleAgedBrieItem(Item item) {
+        if (qualityIsNotMoreThen50(item)) {
+            increaseQualityByOne(item);
+        }
+
+        decreaseSellInByOne(item);
+
+        if (sellByDateHasPassed(item)) {
+            if (qualityIsNotMoreThen50(item)) {
+                increaseQualityByOne(item);
+            }
+        }
+    }
+
+    private void handleBackstagePassesItem(Item item) {
+        if (qualityIsNotMoreThen50(item)) {
+            increaseQualityByOne(item);
+            if (sellIn10DaysOrLess(item)) {
+                if (qualityIsNotMoreThen50(item)) {
+                    increaseQualityByOne(item);
+                }
+            }
+            if (sellIn5DaysOrLess(item)) {
+                if (qualityIsNotMoreThen50(item)) {
+                    increaseQualityByOne(item);
+                }
+            }
+        }
+
+        decreaseSellInByOne(item);
+
+        if (sellByDateHasPassed(item)) {
+            item.quality = 0;
+        }
+    }
+
+    private void handleDefaultItem(Item item) {
+        if (qualityIsNotNegative(item)) {
+            decreaseQualityByOne(item);
+        }
+
+        decreaseSellInByOne(item);
+
+        if (sellByDateHasPassed(item)) {
+            if (qualityIsNotNegative(item)) {
+                decreaseQualityByOne(item);
+            }
+        }
+    }
+
+    private void handleSulfurasItem(Item item) {
+        // Empty
+    }
+
+    // Other commands
     private void decreaseQualityByOne(Item item) {
         item.quality = item.quality - 1;
     }
@@ -72,6 +89,7 @@ class GildedRose {
         item.quality = item.quality + 1;
     }
 
+    // Queries
     private boolean isOfAgedBrieCategory(Item item) {
         return item.name.equals("Aged Brie");
     }
@@ -84,7 +102,7 @@ class GildedRose {
         return item.name.equals("Sulfuras, Hand of Ragnaros");
     }
 
-    static boolean qualityIsNotMoreThen50(Item item) {
+    private boolean qualityIsNotMoreThen50(Item item) {
         return item.quality < 50;
     }
 
